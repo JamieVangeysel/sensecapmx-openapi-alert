c93af55d789d7b53dbc0b7b846c51e7b07e854ed
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2022-04-05 18:38:40 +0200

    fixed alerts

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,26 @@ import { config } from './config'
 import { mail } from './mail'
 import * as sensecap from './sensecap'
 
+const deep_value = (o, p) => p.split('.').reduce((a, v) => a[v], o)
+
 export const main = async () => {
   for (const device of config.devices) {
     const resp = await sensecap.get(device)
     // console.log(device, resp)
     config.alerts.filter(e => e.device === device).forEach(async (alert) => {
       if (alert.condition.value) {
-        if (resp[alert.condition.field] == alert.condition.value) {
+        if (deep_value(resp, alert.condition.field) == alert.condition.value) {
           const message = `${alert.condition.field} is ${alert.condition.value}.`
-          return
+          console.debug(alert.recipient, 'alert triggered SenseCAP', message)
+          // return
           await mail(alert.recipient, 'alert triggered SenseCAP', message)
         }
       } else if (alert.condition.valueNot) {
-        if (resp[alert.condition.field] != alert.condition.valueNot) {
+
+        if (deep_value(resp, alert.condition.field) != alert.condition.valueNot) {
           const message = `${alert.condition.field} is not ${alert.condition.valueNot}.`
-          return
+          console.debug(alert.recipient, 'alert triggered SenseCAP', message, deep_value(resp, alert.condition.field))
+          // return
           await mail(alert.recipient, 'alert triggered SenseCAP', message)
         }
       } else {
